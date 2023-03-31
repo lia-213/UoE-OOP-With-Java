6d0991f99d1bd5a63bac1eb3f36163eed201bb8e
lia-213 <15075156+lia-213@users.noreply.github.com>
2023-03-31 07:47:37 +0100

    Add files via upload

---
+++ JavaLabsWeek1/src/main/java/Multiplier.java
@@ -0,0 +1,9 @@
+public class Multiplier {
+    public static void main(String[] args) {
+        int myNumber = Integer.parseInt(args[0]);
+        int myNumber1 = Integer.parseInt(args[1]);
+        int myNumber2 = Integer.parseInt(args[2]);
+
+        System.out.println(myNumber *  myNumber1 * myNumber2);
+    }
+}
---
+++ JavaLabsWeek1/src/main/java/PersonalGreeting.java
@@ -0,0 +1,6 @@
+public class PersonalGreeting {
+    public static void main(String[] args) {
+        String first_name = args[0], second_name = args[1];
+        System.out.printf("Hello %s %s", first_name, second_name);
+    }
+}
